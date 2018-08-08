6d1c38bb08f3fb22720b456477c55b581725c7c5
changkaitairan <changkaitairan@qq.com>
2018-08-09 01:32:54 +0800

    commit

---
+++ src/sort/XuanzeDemo.java
@@ -0,0 +1,40 @@
+package sort;
+
+import java.util.Scanner;
+
+public class XuanzeDemo {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("请输入一串数字，用空格隔开");
+		String input = sc.nextLine();
+		String[] arr = input.split(" ");
+		int[] arr2 = new int[arr.length];
+		for (int i = 0; i < arr.length; i++) {
+			arr2[i] = Integer.parseInt(arr[i]);
+		}
+		System.out.println("数据输入完毕，处理中...");
+		xuanze(arr2);
+		printArr(arr2);
+		sc.close();
+	}
+
+	public static void xuanze(int[] input) {
+		for (int i = 0; i < input.length - 1; i++) {
+			for (int j = i; j < input.length - 1; j++) {
+				if (input[i] > input[j + 1]) {
+					int temp = input[i];
+					input[i] = input[j + 1];
+					input[j + 1] = temp;
+				}
+			}
+		}
+	}
+
+	public static void printArr(int[] arr) {
+		System.out.print("[");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + ",");
+		}
+		System.out.print("]");
+	}
+}
