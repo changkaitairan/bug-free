0584d905785864423413b130d4859090117fb684
changkaitairan <changkaitairan@qq.com>
2018-08-08 15:30:07 +0800

    commit

--- src/sort/MaopaoDemo.java
+++ src/sort/MaopaoDemo.java
@@ -7,25 +7,33 @@ public class MaopaoDemo {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("请输入一串数字，用空格隔开");
 		String input = sc.nextLine();
-		String [] arr = input.split(" ");
+		String[] arr = input.split(" ");
 		int[] arr2 = new int[arr.length];
-		for(int i = 0; i<arr.length; i++) {
+		for (int i = 0; i < arr.length; i++) {
 			arr2[i] = Integer.parseInt(arr[i]);
 		}
 		System.out.println("数据输入完毕，处理中...");
 		maopao(arr2);
 		printArr(arr2);
 	}
-	
+
 	public static void maopao(int[] input) {
-		
+		for(int i = 0; i<input.length-1; i++) {
+			for(int j = 0; j<input.length-1-i;j++) {
+				if(input[j]>input[j+1]) {
+					int temp = input[j];
+					input[j] = input[j+1];
+					input[j+1] = temp;
+				}
+			}
+		}
 	}
-	
+
 	public static void printArr(int[] arr) {
 		System.out.print("[");
-		for(int i = 0; i< arr.length; i++) {
-			System.out.print(arr[i]+",");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + ",");
 		}
-		System.out.print("]");  
+		System.out.print("]");
 	}
 }
