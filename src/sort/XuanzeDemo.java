382bc3d4f182e112ae08cce0605dab419313a138
changkaitairan <changkaitairan@qq.com>
2018-08-09 10:58:41 +0800

    improve

--- src/sort/XuanzeDemo.java
+++ src/sort/XuanzeDemo.java
@@ -20,11 +20,11 @@ public class XuanzeDemo {
 
 	public static void xuanze(int[] input) {
 		for (int i = 0; i < input.length - 1; i++) {
-			for (int j = i; j < input.length - 1; j++) {
-				if (input[i] > input[j + 1]) {
+			for (int j = i+1; j < input.length; j++) {
+				if (input[i] > input[j]) {
 					int temp = input[i];
-					input[i] = input[j + 1];
-					input[j + 1] = temp;
+					input[i] = input[j];
+					input[j] = temp;
 				}
 			}
 		}
